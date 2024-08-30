98a8a468a91dfe8bb5781a4d51b98f098d87302a
ge0nmo <ands0927@naver.com>
2024-08-30 18:09:28 +0900

    [Refactor] config change

--- src/main/java/com/cpayusin/config/WebConfig.java
+++ src/main/java/com/cpayusin/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.cpayusin.config;
 import com.cpayusin.visitor.controller.port.VisitorInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,20 @@ public class WebConfig implements WebMvcConfigurer
     public void addInterceptors(InterceptorRegistry registry)
     {
         registry.addInterceptor(visitorInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry)
+    {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000", "http://www.cpayusin.com.s3-website.ap-northeast-2.amazonaws.com",
+                        "https://cpayusin.com",
+                        "https://www.cpayusin.com",
+                        "https://dxpqbs6f37n1l.cloudfront.net",
+                        "http://13.124.241.118:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
     }
 }
